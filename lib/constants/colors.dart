e822f44a87e8c85f3f6741c9bd23bf69ada52c63
Joyal Joseph <joyaljosephnote@gmail.com>
2023-09-24 16:07:21 +0530

    student recod all constants colors and screen size adde

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kbackgroundColor = Colors.black;
+const kwhite = Colors.white;
+const kblack = Colors.black;
+const kgrey = Colors.grey;
+const kbuttonColor = Color(0xFFFF3D08);
---
+++ lib/constants/space.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const kheight10 = 10;
+const kheight20 = 20;
+const kheight30 = 30;
+const kwidth10 = 10;
+const kwidth20 = 20;
+const kwidth30 = 30;
+
+final kRadious10 = BorderRadius.circular(10);
+final kRadious20 = BorderRadius.circular(20);
